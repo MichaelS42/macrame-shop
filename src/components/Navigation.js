c354e9a8766ac6e8adae1ee78f045106a34f22e1
Michael Sanchez <michaelsanchez42@live.com>
2022-02-21 14:43:53 -0700

    added nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+//CONTEXT
+import { CartContext } from '../Contexts/CartContext';
+
+const Navigation = () => {
+
+	const { cart } = useContext(CartContext);
+
+	return (
+		<div className="navigation">
+			<NavLink to="/">Products</NavLink>
+			<NavLink to="/cart">
+				Cart <span>{cart.length}</span>
+			</NavLink>
+		</div>
+	);
+};
+
+export default Navigation;
