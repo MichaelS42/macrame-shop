af7e6242c36727b00da773cd6c672afe0199f9db
Michael Sanchez <michaelsanchez42@live.com>
2022-03-08 21:11:40 -0700

    connected nav link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Home =  () => {
-    return (
-        <>
-        <div> H2 from HOME</div>
-        <NavLink to="/navigation">Navigation</NavLink>
-        </>
-    )
-
-} 
-export default Home
+const Home = () => {
+  return (
+    <>
+      <div> H2 from HOME</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/navigation">Navigation</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+export default Home;
